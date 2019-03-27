e52b48ee9776b1e57731cd7d49a02f4b66260ac5
Barbara Makowiec <barbara.makowiec@wp.pl>
2019-03-27 21:45:59 +0100

    new project chat

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+    const environment = env || 'production';
+    const plugins = [new HtmlWebpackPlugin({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })];
+
+    return {
+        mode: environment,
+        entry: './client/index.js',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [{
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: plugins,
+        devServer: {
+            proxy: {
+                '/socket.io': {
+                    target: 'http://localhost:3000',
+                    ws: true
+                }
+            }
+        }
+    }
+}
